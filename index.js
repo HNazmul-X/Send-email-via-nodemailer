a8a2a16234865680723958def99c9978c6caf87a
hnazmul <nazmul.w3@gmail.com>
2021-07-10 13:57:22 +0600

    email setting added

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const PORT = process.env.PORT || 8080
 
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser())
 
-
-console.log(process.env.GMAIL_PASSWORD);
 
 app.get("/", (req, res)=> {
     res.send(" <h1>Hello, Don't Worry I am working</h1> ")
@@ -29,11 +27,25 @@ app.post("/sendEmail", (req, res)=> {
         },
     });
 
+    console.log(req.body)
+    
+
     const mailOptions = {
         from: "nazmul.sarlex@gmail.com",
-        to: `jesmin.w3@gmail.com`,
-        subject: "Massage from your web-hnazmul.com website",
-        html: "<h1>Hi Smartherd</h1><p>Your Messsage</p>",
+        to: `${req.body.email}`,
+        subject: `Portfolio Email From ${req.body?.name}`,
+        html: `
+                  <div style="width:600px;padding:20px;border-radius: 20px; margin:auto;margin-top: 50px; background:lightskyblue; font-family:montserrat, Arial, Helvetica, sans-serif ">
+                    <h1>Hello <br> I am <em><b style="color:yellow">${req.body?.name}</b></em></h1>
+
+                    <article style="margin-top:20px">
+                        <p style="font-size: 14px;">${req.body?.massage}</p>
+                    </article>
+                    
+                </div>
+
+        
+        `,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
